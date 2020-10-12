e32da6cfd5c8650d64e35453f5108a45959dc2c5
Ivan <ivan@example.com>
2020-10-11 21:50:08 -0400

    Update CSS

--- public/script.js
+++ public/script.js
@@ -1,3 +1,14 @@
 if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
 }
+
+function copyToClipboard() {
+    const fieldEl = document.getElementById('main-result');
+
+    var textArea = document.createElement("textarea");
+    textArea.value = fieldEl.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+}
