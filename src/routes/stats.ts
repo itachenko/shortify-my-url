308b56f676bdc218f11e8344b788671a01aebfe4
Ivan <ivan@example.com>
2020-10-28 00:27:32 -0400

    Remove isValid check from stats route; Add message is short url not found

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -7,16 +7,18 @@ import { redis } from "../modules/redis";
 const router = Router();
 
 router.post("/", validateUrl, async (req: Request, res: Response) => {
-  if (req.body.isValid === false) {
-    return res.redirect("/");
-  }
-
   const sessionId = req.session?.id as string;
   const sessionData = {} as ISessionData;
 
   const key: string = req.body.url.split("/").slice(-1)[0];
   const clicksCount: number | null = await redis.getClicksCount(key);
-  if (clicksCount !== null) {
+  if (clicksCount === null) {
+    sessionData.errorMessage = "Such short URL was not found in the system";
+    await redis.setSessionData(
+      req.session?.id as string,
+      JSON.stringify(sessionData)
+    );
+  } else {
     sessionData.statsObject = {
       url: req.body.url,
       clicksCount,
