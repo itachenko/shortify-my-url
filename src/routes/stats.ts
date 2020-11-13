fb286eb82789b5234c6a8942902815185ee20d36
Ivan <ivan@example.com>
2020-11-13 18:26:40 -0500

    Add cors for /api/stats route

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
 import { validateUrl } from "../middleware/url";
 import { redis } from "../modules/redis";
+import cors from "cors";
+import { corsOptions } from "../corsOptions";
 
 const router = Router();
 
@@ -12,20 +14,25 @@ router.options("/", (req: Request, res: Response) => {
   return res.status(200).send();
 });
 
-router.post("/", validateUrl, async (req: Request, res: Response) => {
-  const key: string = req.body.url.split("/").slice(-1)[0];
-  const clicksCount: number | null = await redis.getClicksCount(key);
-  if (clicksCount === null) {
-    return res
-      .header("Access-Control-Allow-Origin", "*")
-      .status(500)
-      .send({ error: "Such short URL was not found in the system" });
-  } else {
-    return res
-      .header("Access-Control-Allow-Origin", "*")
-      .status(200)
-      .send({ url: req.body.url, clicksCount });
+router.post(
+  "/",
+  cors(corsOptions),
+  validateUrl,
+  async (req: Request, res: Response) => {
+    const key: string = req.body.url.split("/").slice(-1)[0];
+    const clicksCount: number | null = await redis.getClicksCount(key);
+    if (clicksCount === null) {
+      return res
+        .header("Access-Control-Allow-Origin", "*")
+        .status(500)
+        .send({ error: "Such short URL was not found in the system" });
+    } else {
+      return res
+        .header("Access-Control-Allow-Origin", "*")
+        .status(200)
+        .send({ url: req.body.url, clicksCount });
+    }
   }
-});
+);
 
 export default router;
