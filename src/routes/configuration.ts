eaacd7af4560ad7b631204580e94a256aca6ed55
Ivan <ivan@example.com>
2020-11-20 23:45:31 -0500

    Attempt to build frontend in prod

--- src/routes/configuration.ts
+++ src/routes/configuration.ts
@@ -1,10 +1,8 @@
 import { Request, Response, Router } from "express";
-import { corsOptions } from "../corsOptions";
-import cors from "cors";
 
 const router: Router = Router();
 
-router.get("/", cors(corsOptions), (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
   return res.header("Access-Control-Allow-Origin", "*").status(200).send({
     shortUrlTTL: process.env.SHORT_URL_TTL_DAYS,
     requestRateLimitCount: process.env.REQUEST_RATE_LIMIT_COUNT,
